f2022c720b0c322ab27d135eb839bb39a8eb6a92
NodirPortfolio <154463843+NodirPortfolio@users.noreply.github.com>
2024-08-12 19:12:06 +0500

    Update send_mail.php

--- send_mail.php
+++ send_mail.php
@@ -4,7 +4,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = htmlspecialchars($_POST['email']);
     $message = htmlspecialchars($_POST['message']);
 
-    $to = "nodirmustafoyev13@gmail.com"; // Replace with your email address
+    $to = "sanjarmustafaev155@gmail.com"; // Replace with your email address
     $subject = "New Contact Form Submission";
     $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
     $headers = "From: $email";
